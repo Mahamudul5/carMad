284656f42752d5c73f42a7f4ffaeb21e283988dd
mahamudul5 <mh.bilash22@gmail.com>
2022-05-28 19:53:36 +0600

    blogs updated

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -8,43 +8,70 @@ const Blog = () => {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Athentication vs Authorization?
+                            1.  How you will improve the performance of react?
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            <h6>Authentication:</h6>
-                            Authentications means to the activity a party makes to verify anothers party. such as a persons told to their identity.Authntication is  using differents methods by the verifying company.another form of authentiction is contine authentication which uses contextual information on the user and circumstances, all  kind of the risk at hand.
-                            <h6>Authorization:</h6>
-                            Authorization is a security mechanism used to determine user/client privileges or access levels related to system resources, including computer programs, files, services, data and application features. Authorization is normally preceded by authentication for user identity verification. System administrators (SA) are typically assigned permission levels covering all system and user resources.
+                            Keeping component state local where necessary.
 
-                            During authorization, a system verifies an authenticated user's access rules and
+                            Memoizing React components to prevent unnecessary re-renders.
+
+                            Code-splitting in React using dynamic import()
+
+                            Windowing or list virtualization in React.
+
+                            Lazy loading images in React.
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Why using Firebase? what other option to implement Authentication?
+                            2.  What are the different way to manage a state of react application?
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            we basically use firebase for authentication,hosting, build web, game, ios software development kit.
-                            if i not use firebase , i can use Parse for backend
-                            then, Back4app-parse for hosting.And BackendLess- for mobile backend & api services.We can also use kuzzel-Backend for web projects ,so on...
+                            1. Data State. ... 2. Control State. ... 3. Session State. ... 4. Location State. ...
                         </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            What other services without authentication firebase provide?
+                            3.   how does prototpycal inheritance work?
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            Firebase is a Backend-as-a-Service (Baas). It provides developers with a variety of tools and services to help them develop quality apps, grow their user base, and earn profit. It is built on Google's infrastructure. Firebase is categorized as a NoSQL database program, which stores data in JSON-like documents.
+                            The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            4.   You have an array of products.Each object has a name ,price , description etc.How will you implement a search to find products by name?
+                        </button>
+                    </h2>
+                    <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            1. If I need the index of the found element in the array, use findIndex() . 2. If I need to find the index of a value, use Array.prototype.indexOf() . ... 3. If I need to find if a value exists in an array, use Array.prototype.includes() .
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseTwo">
+                            5.   What is a unit test ? why should write unit tests?
+                        </button>
+                    </h2>
+                    <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            Unit testing is a software development process in which the smallest testable parts of an application, called units, are individually and independently scrutinized for proper operation. This testing methodology is done during the development process by the software developers and sometimes QA staff.
+
+                            One of the benefits of unit tests is that they isolate a function, class or method and only test that piece of code. Higher quality individual components create overall system resiliency. Thus, the result is reliable code. Unit tests also change the nature of the debugging process
                         </div>
                     </div>
                 </div>
